aa9233d88a76704e1afed8c76c4b6d161ab8aa0c
Andre Moraes <andrebq@gmail.com>
2012-04-30 20:44:02 -0300

    Remove the required port parameter in the script

--- compile-and-run.sh
+++ compile-and-run.sh
@@ -3,4 +3,13 @@
 go clean
 go build ./...
 go build .
-./getdone -port $1 -root ./web/site/
+
+port=$1
+
+if [ "z" == "z$1" ]; then
+	port=:"8888"
+fi
+
+if [ -x "./getdone" ]; then
+	./getdone -port $port -root ./web/site/
+fi
